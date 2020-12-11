8011340e661b32f6fd4d96dec90ec650a1b1c66c
Linjiangzhu <yuyuex97@gmail.com>
2020-12-10 19:48:07 -0800

    put actual ytb footage, member photo, add text description

--- layout/AboutPageLayout/index.js
+++ layout/AboutPageLayout/index.js
@@ -5,28 +5,28 @@ const AboutPageLayout = () => {
   const members = [
     {
       name: "Colin Tran",
-      avatar: "/avatar-placeholder.jpg",
-      description: "Team member",
+      avatar: "/avatar/Colin.png",
+      description: "Game developer",
     },
     {
       name: "Isabelle Beckmann",
-      avatar: "/avatar-placeholder.jpg",
-      description: "Team member",
+      avatar: "/avatar/Isabelle.png",
+      description: "Game developer",
     },
     {
       name: "Rionel Dmello",
-      avatar: "/avatar-placeholder.jpg",
-      description: "Team member",
+      avatar: "/avatar/Rio.png",
+      description: "Game developer",
     },
     {
       name: "Richard Lopez",
-      avatar: "/avatar-placeholder.jpg",
-      description: "Team member",
+      avatar: "/avatar/Richard.png",
+      description: "Game developer",
     },
     {
       name: "Yue Yu",
-      avatar: "/avatar-placeholder.jpg",
-      description: "Team member",
+      avatar: "/avatar/Yue.png",
+      description: "Game developer",
     },
   ];
   return (
@@ -40,13 +40,24 @@ const AboutPageLayout = () => {
           Ace Entertainment is a game group from UCI ICS 113 course. Our mission
           is to build fancy, exciting game world to explore and conquer.
         </p>
+        <br />
+        <p>
+          We created Escaping Eve entirely through virtual meetings. Through
+          weekly Discord meetings and Zoom calls, we developed Escaping Eve
+          using Unity. We decided to make a platformer game because we were all
+          inspired by Nintendo's Mario games. Since platformer games are common,
+          our main goal in the creation of Escaping Eve was to create a game
+          that provides players with a unique experience. We accomplished this
+          goal with our space theme and designing our main character, Rex, to be
+          able to manipulate gravity and flip upside down.
+        </p>
         <h1>THE TEAM</h1>
         <div className={styles["team-list"]}>
-          {members.map((m) => (
-            <div className={styles["team-member"]}>
-              <img src={m.avatar} />
+          {members.map((m, idx) => (
+            <div key={idx} className={styles["team-member"]}>
+              <img src={m.avatar} height="150px" />
               <h3>{m.name}</h3>
-              {m.description}
+              <p>{m.description}</p>
             </div>
           ))}
         </div>
--- layout/HomePageLayout/index.js
+++ layout/HomePageLayout/index.js
@@ -42,28 +42,46 @@ const HomePageLayout = () => {
         </div>
       </div>
       <div className={styles["game-info"]}>
-        <div className={styles["game-info-about"]}>
-          <div className={styles["img-wrapper"]}>
-            <img src="/game-portrait.png" />
-          </div>
-          <h2>About the game</h2>
-          <p>
-            <b>Escape Eve</b> is a space themed platformer game. Astronaut, Rex,
-            crashed on a deserted planet called Eve. Rex has to find out a way
-            to escape this dangerous place.
-          </p>
-          <hr />
-          <h2>Will be available on:</h2>
-          <p>Steam, Epic, Nintendo Switch</p>
-        </div>
-        <div className={styles["game-info-review"]}>
-          <h2>Game Reviews</h2>
-          {reviews.map((r) => (
-            <div>
-              <span className={styles["comment"]}>{r.comment}</span>
-              <span className={styles["reviewer"]}>{r.reviewer}</span>
+        <div className={styles["upperside"]}>
+          <div className={styles["game-info-about"]}>
+            <div className={styles["img-wrapper"]}>
+              <img src="/game-portrait.png" />
             </div>
-          ))}
+            <h2>About the game</h2>
+            <p>
+              <b>Escape Eve</b> is a space themed adventure platformer game. To
+              win Escaping Eve, you must get Rex the astronaut to the rescue
+              spaceship as quickly as possible because Eve is crumbling into
+              pieces. Whatever you do, don't fall.
+            </p>
+            <hr />
+            <h2>Will be available on:</h2>
+            <p>Steam, Epic, Nintendo Switch</p>
+          </div>
+          <div className={styles["game-info-review"]}>
+            <h2>Game Reviews</h2>
+            {reviews.map((r, idx) => (
+              <div key={idx}>
+                <span className={styles["comment"]}>{r.comment}</span>
+                <span className={styles["reviewer"]}>{r.reviewer}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles["downside"]}>
+          <h2>The Creation of Rex and Eve</h2>
+          <p>
+            Rex was an astronaut sent from Earth to complete a task on another
+            planet, however, he never completed his mission. Rex crashed on a
+            deserted planet known as Eve. Rex's ship and Eve were destroyed on
+            impact. Rex was able to call for help and a rescue ship was sent
+            from a nearby planet to pick up Rex, but now Rex must escape Eve
+            without falling and getting sucked into a blackhole. Rex can run
+            fast, but he must be able to handle navigating the moving rock
+            structures to get to the rescue ship. What makes Escaping Eve unique
+            is our character's ability to manipulate physics and flip upside
+            down to walk on the structures above him.
+          </p>
         </div>
       </div>
       <div className={styles["game-footage"]}>
@@ -75,17 +93,17 @@ const HomePageLayout = () => {
             width: "100%",
             height: "56vw",
           }}
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          frameborder="0"
+          src="https://www.youtube.com/embed/tOwQ1PJBnQY"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
       <div className={styles["game-gallery"]}>
         <h1>Gallery</h1>
         <div className={styles["image-list"]}>
-          {screenshots.map((url) => (
-            <a href={url} target="_blank">
+          {screenshots.map((url, idx) => (
+            <a key={idx} href={url} target="_blank">
               <img src={url}></img>
             </a>
           ))}
